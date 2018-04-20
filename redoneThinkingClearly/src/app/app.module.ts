fa4da8f6731a2fd849cd52456bfdb0796a603541
wildaces215 <wannabecoastie@gmail.com>
2018-04-20 14:03:01 -0400

    Update app.module.ts

--- redoneThinkingClearly/src/app/app.module.ts
+++ redoneThinkingClearly/src/app/app.module.ts
@@ -25,15 +25,9 @@ import 'rxjs/add/operator/map';
 import { ComponentsModule } from '../components/components.module';
 import { SliderPage } from '../pages/slider/slider';
 
-//Plugins
-//firebase config would go here taken out for security purposes
+
 export const firebaseConfig= {
-  apiKey: "AIzaSyBnAtpWuFA-4z6zmkByQPHOTmVYrUnHik8",
-  authDomain: "thinkingclearly-32938.firebaseapp.com",
-  databaseURL: "https://thinkingclearly-32938.firebaseio.com",
-  projectId: "thinkingclearly-32938",
-  storageBucket: "thinkingclearly-32938.appspot.com",
-  messagingSenderId: "816024632443"
+ //Creditonals for database go here 
 };
 
 
